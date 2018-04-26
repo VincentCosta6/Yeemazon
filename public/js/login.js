2fcdd87b963ce1be0dcb63bfe0f437b8ea36706d
m-huff <michaelhuff17@gmail.com>
2018-04-26 13:28:15 -0700

    Consolidate signup.js code into login.js

--- public/js/login.js
+++ public/js/login.js
@@ -21,6 +21,7 @@ function sendSignup()
 	newDiv = "<div id=\"loginBox\" class=\"2\"><br><label>Choose a username</label><div style=\"margin-top: 10px\"></div><input id=\"username\" type=\"text\" class=\"loginInput\"><div style=\"margin-top: 30px\"></div><label>Enter your email</label><div style=\"margin-top: 10px\"></div><input id=\"email\" name=\"email\" type=\"text\" class=\"loginInput\"><div style=\"margin-top: 30px\"></div><label>Choose a password (hidden)</label><div style=\"margin-top: 10px\"></div><input id=\"password\" type=\"password\" class=\"loginInput\"><div style=\"margin-top: 40px\"></div><div align = \"center\" class=\"g-recaptcha\" data-sitekey=\"6LfhUk4UAAAAACFLoT8YhRJxxtEJuamEWGQXNhfb\"></div><div style=\"margin-top: 30px\"></div><button id=\"request\" class=\"request\">Sign up!</button><div style=\"margin-top: 10px\"><button id=\"back\" class=\"back\">Go Back</button></div>";
 	$(".1").replaceWith(newDiv);
 	$("#back").click(sendBack);
+   $("#request").click(signup);
 }
 function sendBack()
 {
@@ -28,11 +29,15 @@ function sendBack()
 	newDiv = "<div id=\"loginBox\" class=\"1\"><br><label>Username</label><div style=\"margin-top: 10px\"></div><input id=\"username\" type=\"text\" class=\"loginInput\"><div style=\"margin-top: 30px\"></div><label>Password</label><div style=\"margin-top: 10px\"></div><input id=\"password\" type=\"password\" class=\"loginInput\"><div style=\"margin-top: 25px\"></div><button id=\"request\" class=\"request\">Log In</button><div style=\"margin-top: 20px\"></div><label>Don't have an account?</label><button id=\"signup\" class=\"request\">Sign up!</button><div style=\"margin-top: 10px\"></div></div>";
 	$(".2").replaceWith(newDiv);
 	$("#signup").click(sendSignup);
+   $("#request").click(sendLogin);
 }
 $(document).ready(function(){
 	$.get("/userInfo", success);
-	$("#request").click(sendLogin);
 	$("#signup").click(sendSignup);
+
+   function submitForm(e) {
+	   e.preventDefault();
+   }
 
 	$('.loginInput').keypress(function(e){
       if(e.keyCode==13)
@@ -45,8 +50,7 @@ function success(data)
 		window.location = window.location.href.split("/")[1] + "/session";
 }
 
-/*
-function sendLogin()
+function signup()
 {
 	$.ajax({
             url:"/signup",
@@ -56,31 +60,4 @@ function sendLogin()
             dataType : "json"
         });
 }
-function redirect(data)
-{
-	if(data.status)
-		alert(data.status);
-	else
-		window.location = window.location.href.split("/")[0] + data.redirect;
-}
-$(document).ready(function(){
-	$.get("/userInfo", success);
-	$("#request").click(sendLogin);
-	//$("#former").on('submit', submitForm);
 
-	$(document).keypress(function(e){
-      if(e.keyCode==13)
-      $('#request').click();
-    });
-});
-function submitForm(e)
-{
-	e.preventDefault();
-
-}
-function success(data)
-{
-	if(!data.redirect)
-		window.location = window.location.href.split("/")[1] + "/session";
-}
-*/
