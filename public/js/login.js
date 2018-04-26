2a2fad157dae1efa6387177cde76a8a8994fbd49
Michael Huff-373000822 <373000822@vusdnoc.vusd.k12.ca.us>
2018-04-26 13:02:07 -0700

    oops

--- public/js/login.js
+++ public/js/login.js
@@ -44,3 +44,43 @@ function success(data)
 	if(!data.redirect)
 		window.location = window.location.href.split("/")[1] + "/session";
 }
+
+/*
+function sendLogin()
+{
+	$.ajax({
+            url:"/signup",
+            type:"POST",
+            data: {username:$("#username").val(), email:$("#email").val(), password:$("#password").val(), captcha:$("#g-recaptcha-response").val()},
+            success: redirect,
+            dataType : "json"
+        });
+}
+function redirect(data)
+{
+	if(data.status)
+		alert(data.status);
+	else
+		window.location = window.location.href.split("/")[0] + data.redirect;
+}
+$(document).ready(function(){
+	$.get("/userInfo", success);
+	$("#request").click(sendLogin);
+	//$("#former").on('submit', submitForm);
+
+	$(document).keypress(function(e){
+      if(e.keyCode==13)
+      $('#request').click();
+    });
+});
+function submitForm(e)
+{
+	e.preventDefault();
+
+}
+function success(data)
+{
+	if(!data.redirect)
+		window.location = window.location.href.split("/")[1] + "/session";
+}
+*/
