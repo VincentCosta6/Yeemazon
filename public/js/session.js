6ff08c78c3f9eb18e2c261e97337e578b30680c0
Michael Huff-373000822 <373000822@vusdnoc.vusd.k12.ca.us>
2018-05-01 13:03:36 -0700

    Search is a little broken

--- public/js/session.js
+++ public/js/session.js
@@ -1,3 +1,4 @@
+//State determines the size of the toolbar; 0 when on a session page, 1 when on a specific item page
 var state = 0;
 
 $(document).ready(function() {
@@ -12,19 +13,24 @@ $(document).ready(function() {
     window.location = window.location.href.split("/")[1] + "/account";
   });
 
-  //CART CODE
+  //CART CLICK METHOD
   $("#cart").click(() => {
     if (state) {
+      //CHANGE TOOLBAR BACK TO NORMAL IF NECESSARY
       $("#fakeToolbar").attr("id", "anotherFakeToolbar");
       state = 0;
       $("#userGreeting").css("transform", "translateX(0px)");
       $("#userGreeting").css("animation", "slideBack 1s 1");
     }
-    var newDiv = "<div id=\"ordersPageback\"><h2 class=\"itemLabel\" id=\"cartNumber\"style=\"padding-top:30px;\">Your Cart ({NUMBER} Items)</h2><div style=\"margin-top: 10px\"></div><div class=\"orderHolder\"></div></div>";
-    $("#searchBack").replaceWith(newDiv);
-    $("#sessionPageback").replaceWith(newDiv);
-    $("#itemPageback").replaceWith(newDiv);
 
+    //LOAD CART HTML
+    var divs = ["#searchBack", "#sessionPageback", "#itemPageback"];
+    for (let i in divs) {
+      $(divs[i]).replaceWith("<div id=\"ordersPageback\"><div>");
+      $("#ordersPageback").load("css/elements.html #ordersPageback>*");
+    }
+
+    //CREATE CART ITEMS
     $.get("/cartItems", function(data) {
       for (var i = 0; i < data.items.length; i++) {
 
@@ -32,16 +38,22 @@ $(document).ready(function() {
         var divCreator = "<div id=\"" + id + "\" class=\"itemBox\"><img id = \"" + id + "imger" + "\"src=\"" + data.items[i].link + "\" style=\"cursor:pointer;width:140px;height:140px;margin-top:5px\"></img><br><label>" + data.items[i].name + "</label><br><label>$" + data.items[i].price + "</label><br><label class=\"remove\" id = \"" + id + "remover\" style = \"cursor:pointer;\">Remove</label></div>";
         $(".orderHolder").append(divCreator);
         var redirect = function() {
-          //INDIVIDUAL ITEM PAGE CODE
+
+          //LOAD ITEM PAGE HTML
           state = 1;
-          var newDiv = "<div id=\"itemPageback\"><h2 id=\"name\" class=\"itemLabel\" style=\"padding-top:30px;padding-left:40px\">%ITEM%</h2><div style=\"margin-top: 30px\"></div><div id=\"holder\" class=\"bigHolder\"><div class=\"infoHolder\"><label id=\"itemPrice\" style=\"margin-left:15px;padding-top:25px;font-size:48px\">%PRICE%</label><label id=\"shipping\" style=\"padding-top:5px;font-size:12px\">%SHIPPING%</label><div style=\"margin-top: 30px\"></div><label id=\"itemDesc\" style=\"margin-left:15px\">%DESC%</label><div style=\"margin-top: 40px\"></div><label id=\"itemClick\" style=\"margin-left:15px\">%INFO%</label><div style=\"margin-top: 10px\"></div><label id=\"itemClickNum\" style=\"margin-left:15px\">%INFO%</label><div style=\"margin-top: 10px\"></div><label id=\"itemOrders\" style=\"margin-left:15px\">%INFO%</label><div style=\"margin-top: 10px\"></div><label id=\"itemID\" style=\"margin-left:15px\">%ID%</label></div><button id=\"addToCart\" class=\"bigButton\">ADD TO CART</button></div></div>";
-          $("#ordersPageback").replaceWith(newDiv);
-          $("#sessionPageback").replaceWith(newDiv);
+          var divs = ["#ordersPageback", "#sessionPageback"];
+          for (let i in divs) {
+            $(divs[i]).replaceWith("<div id=\"itemPageback\"><div>");
+            $("#itemPageback").load("css/elements.html #itemPageback>*");
+          }
+
+          //CHANGE TOOLBAR BACK IF NECESSARY
           $("#userGreeting").css("transform", "translateX(465px)");
           $("#userGreeting").css("animation", "slideAcross 1s 1");
           $("#toolbar").attr("id", "fakeToolbar");
           $("#anotherFakeToolbar").attr("id", "fakeToolbar");
 
+          //POPULATE ITEM PAGE WITH DATA
           $.get("/getItemInfo", {
             _id: id
           }, (data) => {
@@ -59,6 +71,7 @@ $(document).ready(function() {
             id = data.item._id;
           });
 
+          //ADD TO CART BUTTON CODE
           $("#addToCart").click(() => {
             $.post("/addToCart", {
               itemID: id
@@ -66,8 +79,9 @@ $(document).ready(function() {
               alert(((data.status) ? "Item added to cart" : "Something went wrong"));
             });
           });
-          //window.location = window.location.href.split("/")[1] + "/item?id=" + id
         };
+
+        //REMOVE FROM CART BUTTON CODE
         var remover = function() {
           $.post("/removeFromCart", {
             itemID: id
@@ -81,32 +95,34 @@ $(document).ready(function() {
         $("#" + id + "remover").click(remover);
       }
 
-      //scale the back of the page to compensate for number of items
+      //SCALE PAGE BACKING FOR NUMBER OF ITEMS
       $(".orderHolder").css("height", 230 * (Math.floor((data.items.length / 6)) + 1));
       $("#ordersPageback").css("height", 350 * (Math.floor((data.items.length / 6)) + 1));
 
-      //update search list size to show user
+      //CART LENGTH DISPLAY
       $(".itemLabel").html("Your cart has " + data.items.length + " items");
 
     });
   });
 
-  //RESET TO BROWSE PAGE / SESSION PAGE
+  //HEADER CLICK CODE
   $(".loginHeader").click(() => {
     if (state) {
+      //RESET TOOLBAR IF NEEDED
       $("#fakeToolbar").attr("id", "anotherFakeToolbar");
       state = 0;
       $("#userGreeting").css("transform", "translateX(0px)");
       $("#userGreeting").css("animation", "slideBack 1s 1");
     }
-    //Unholy line of code, sorry
-    var newDiv = "<div id=\"sessionPageback\"><h2 class=\"itemLabel\" style=\"padding-top:30px\">Popular Items</h2><div style=\"margin-top: 10px\"></div><div class=\"itemHolder\" id=\"items1\"></div><div style=\"margin-top: 30px\"></div><h2 class=\"itemLabel\">Under $20</h2><div style=\"margin-top: 10px\"></div><div class=\"itemHolder\" id=\"items2\"></div><div style=\"margin-top: 30px\"></div><h2 class=\"itemLabel\">Mr. Yee's Picks</h2><div style=\"margin-top: 10px\"></div><div class=\"itemHolder\" id=\"items3\"></div><div style=\"margin-top: 100px\"></div> <h2 class = \"itemLabel\">All Items</h2> <div style = \"margin-top: 10px\"></div> <div class = \"itemCont\" id=\"items4\"> </div></div > ";
-    $("#searchBack").replaceWith(newDiv);
-    $("#sessionPageback").replaceWith(newDiv);
-    $("#ordersPageback").replaceWith(newDiv);
-    $("#itemPageback").replaceWith(newDiv);
-    //populate homepage
-    //grab first 6 "popular items", "under20", and "yee"
+
+    //LOAD SESSION HTML
+    var divs = ["#searchBack", "#sessionPageback", "#ordersPageback", "#itemPageback"];
+    for (let i in divs) {
+      $(divs[i]).replaceWith("<div id=\"sessionPageback\"><div>");
+      $("#sessionPageback").load("css/elements.html #sessionPageback>*");
+    }
+
+    //LOAD SESSION ITEMS
     let searcher = ["popular", "under20", "yee"];
     for (let i in searcher)
       $.get("/findItems", {
@@ -118,6 +134,7 @@ $(document).ready(function() {
             appender(data.items[i2]._id, data.items[i2].link, data.items[i2].name, data.items[i2].price, data.items[i2].clicks, data.items[i2].usersClicked.length, data.appending);
       });
 
+    //LOAD FULL ITEM LIST
     $.get("/findItems", {
       keywords: "cool"
     }, function(data) {
@@ -125,29 +142,32 @@ $(document).ready(function() {
         if (data.items[i])
           appender(data.items[i]._id, data.items[i].link, data.items[i].name, data.items[i].price, data.items[i].clicks, data.items[i].usersClicked.length, 4);
 
+      //SCALE PAGE BACKING FOR NUMBER OF ITEMS
       $(".itemCont").css("height", 260 * (Math.floor((data.items.length / 6)) + 1));
       $("#sessionPageback").css("height", 1100 + (260 * (Math.floor((data.items.length / 6)) + 1)));
     });
   });
 
-  //SEARCH PAGE
+  //SEARCH BUTTON CLICK CODE
   $("#request").click(() => {
     if (state) {
+      //RESET TOOLBAR IF NEEDED
       $("#fakeToolbar").attr("id", "anotherFakeToolbar");
       state = 0;
       $("#userGreeting").css("transform", "translateX(0px)");
       $("#userGreeting").css("animation", "slideBack 1s 1");
     }
     if ($("#search").val() && $("#search").val() !== "Search for an item")
-      var newDiv = "<div id=\"searchBack\"><h2 class=\"itemLabel\" style=\"padding-top:30px\">Search Results for {QUERY}</h2><div style=\"margin-top: 10px\"></div><div class=\"searchHolder\"></div><h3>End of Search Results</h3>  </div>";
-    $("#searchBack").replaceWith(newDiv);
-    $("#sessionPageback").replaceWith(newDiv);
-    $("#itemPageback").replaceWith(newDiv);
-    $("#ordersPageback").replaceWith(newDiv);
+
+    //LOAD SEARCH PAGE HTML
+    var divs = ["#searchBack", "#sessionPageback", "#ordersPageback", "#itemPageback"];
+    for (let i in divs) {
+      $(divs[i]).replaceWith("<div id=\"searchBack\"><div>");
+    }
+    $("#searchBack").load("css/elements.html #searchBack>*");
     $(".itemLabel").html("No search results");
 
-    //populate search page
-
+    //LOAD SEARCHED ITEMS
     $.get("/findItems", {
       keywords: $("#search").val()
     }, function(data) {
@@ -157,16 +177,22 @@ $(document).ready(function() {
         var divCreator = "<div id=\"" + id + "\" class=\"itemBox\"><img src=\"" + data.items[i].link + "\" style=\"width:140px;height:140px;margin-top:5px\"></img><br><label>" + data.items[i].name + "</label><br><label>$" + data.items[i].price + "</label></div>";
         $(".searchHolder").append(divCreator);
         $("#" + id).click(function() {
-          //INDIVIDUAL ITEM PAGE CODE
+
+          //LOAD ITEM PAGE HTML
           state = 1;
-          var newDiv = "<div id=\"itemPageback\"><h2 id=\"name\" class=\"itemLabel\" style=\"padding-top:30px;padding-left:40px\">%ITEM%</h2><div style=\"margin-top: 30px\"></div><div id=\"holder\" class=\"bigHolder\"><div class=\"infoHolder\"><label id=\"itemPrice\" style=\"margin-left:15px;padding-top:25px;font-size:48px\">%PRICE%</label><label id=\"shipping\" style=\"padding-top:5px;font-size:12px\">%SHIPPING%</label><div style=\"margin-top: 30px\"></div><label id=\"itemDesc\" style=\"margin-left:15px\">%DESC%</label><div style=\"margin-top: 40px\"></div><label id=\"itemClick\" style=\"margin-left:15px\">%INFO%</label><div style=\"margin-top: 10px\"></div><label id=\"itemClickNum\" style=\"margin-left:15px\">%INFO%</label><div style=\"margin-top: 10px\"></div><label id=\"itemOrders\" style=\"margin-left:15px\">%INFO%</label><div style=\"margin-top: 10px\"></div><label id=\"itemID\" style=\"margin-left:15px\">%ID%</label></div><button id=\"addToCart\" class=\"bigButton\">ADD TO CART</button></div></div>";
-          $("#sessionPageback").replaceWith(newDiv);
-          $("#searchBack").replaceWith(newDiv);
+          var divs = ["#searchBack", "#sessionPageback"];
+          for (let i in divs) {
+            $(divs[i]).replaceWith("<div id=\"itemPageback\"><div>");
+            $("#itemPageback").load("css/elements.html #itemPageback>*");
+          }
+
+          //SCALE TOOLBAR DOWN
           $("#userGreeting").css("transform", "translateX(465px)");
           $("#userGreeting").css("animation", "slideAcross 1s 1");
           $("#toolbar").attr("id", "fakeToolbar");
           $("#anotherFakeToolbar").attr("id", "fakeToolbar");
 
+          //POPULATE ITEM PAGE WITH DATA
           $.get("/getItemInfo", {
             _id: id
           }, (data) => {
@@ -184,6 +210,7 @@ $(document).ready(function() {
             id = data.item._id;
           });
 
+          //ADD ITEM TO CART CODE
           $("#addToCart").click(() => {
             $.post("/addToCart", {
               itemID: id
@@ -191,9 +218,9 @@ $(document).ready(function() {
               alert(((data.status) ? "Item added to cart" : "Something went wrong"));
             });
           });
-          //window.location = window.location.href.split("/")[1] + "/item?id=" + id
         });
 
+        //FINISH POPULATING SEARCH PAGE WITH DATA
         $(".searchHolder").css("height", 230 * (Math.floor((data.items.length / 6)) + 1));
         $("#searchBack").css("height", 350 * (Math.floor((data.items.length / 6)) + 1));
         $(".itemLabel").html("No search results");
@@ -203,7 +230,7 @@ $(document).ready(function() {
 
   });
 
-  //grab first 6 "popular items", "under20", and "yee"
+  //POPULATE SESSION PAGE WITH ITEMS
   let searcher = ["popular", "under20", "yee"];
   for (let i in searcher)
     $.get("/findItems", {
@@ -215,6 +242,7 @@ $(document).ready(function() {
           appender(data.items[i2]._id, data.items[i2].link, data.items[i2].name, data.items[i2].price, data.items[i2].clicks, data.items[i2].usersClicked.length, data.appending);
     });
 
+  //LOAD FULL ITEM LIST
   $.get("/findItems", {
     keywords: "cool"
   }, function(data) {
@@ -222,11 +250,12 @@ $(document).ready(function() {
       if (data.items[i])
         appender(data.items[i]._id, data.items[i].link, data.items[i].name, data.items[i].price, data.items[i].clicks, data.items[i].usersClicked.length, 4);
 
+    //SCALE PAGE BACKING FOR NUMBER OF ITEMS
     $(".itemCont").css("height", 260 * (Math.floor((data.items.length / 6)) + 1));
     $("#sessionPageback").css("height", 1100 + (260 * (Math.floor((data.items.length / 6)) + 1)));
   });
 
-
+  //SEARCH BUTTON BEHAVIOR
   if (!($("#search").val()) || $("#search").val() == "Search for an item")
     $('#request').prop('hidden', true);
 
@@ -243,6 +272,7 @@ $(document).ready(function() {
   });
 });
 
+//XSS PREVENTION
 $(document).keypress(function(e) {
   if ($("#search").val().includes("<") || $("#search").val().includes(">"))
     return alert("Improper search please dont use < or >");
@@ -255,12 +285,11 @@ $(document).keypress(function(e) {
     $('#request').prop('hidden', false);
 });
 
+//HELPER METHOD FOR APPENDING ITEM DIVS TO PAGE
 function appender(id, link, name, price, clicks, uniqueClicks, which) {
   var divCreator = "<div  id=\"" + id + "\" class=\"itemBox\"><img src=\"" + link + "\" style=\"cursor:pointer;width:140px;height:140px;margin-top:5px\"></img><br><label id=\"boxLabel\">" + name + "</label><br><label id=\"boxLabel\">$" + price + "</label></div>";
   $("#items" + which).append(divCreator);
   $("#" + id).click(function() {
-
-    console.log("BEGIN");
 
     $.post("/itemClicked", {
       itemID: id
@@ -268,15 +297,21 @@ function appender(id, link, name, price, clicks, uniqueClicks, which) {
       console.log("Clicked")
     });
 
-    //INDIVIDUAL ITEM PAGE CODE
+    //LOAD ITEM PAGE HTML
     state = 1;
-    var newDiv = "<div id=\"itemPageback\"><h2 id=\"name\" class=\"itemLabel\" style=\"padding-top:30px;padding-left:40px\">%ITEM%</h2><div style=\"margin-top: 30px\"></div><div id=\"holder\" class=\"bigHolder\"><div class=\"infoHolder\"><label id=\"itemPrice\" style=\"margin-left:15px;padding-top:25px;font-size:48px\">%PRICE%</label><label id=\"shipping\" style=\"padding-top:5px;font-size:12px\">%SHIPPING%</label><div style=\"margin-top: 30px\"></div><label id=\"itemDesc\" style=\"margin-left:15px\">%DESC%</label><div style=\"margin-top: 40px\"></div><label id=\"itemClick\" style=\"margin-left:15px\">%INFO%</label><div style=\"margin-top: 10px\"></div><label id=\"itemClickNum\" style=\"margin-left:15px\">%INFO%</label><div style=\"margin-top: 10px\"></div><label id=\"itemOrders\" style=\"margin-left:15px\">%INFO%</label><div style=\"margin-top: 10px\"></div><label id=\"itemID\" style=\"margin-left:15px\">%ID%</label></div><button id=\"addToCart\" class=\"bigButton\">ADD TO CART</button></div></div>";
-    $("#sessionPageback").replaceWith(newDiv);
+    var divs = ["#sessionPageback"];
+    for (let i in divs) {
+      $(divs[i]).replaceWith("<div id=\"itemPageback\"><div>");
+      $("#itemPageback").load("css/elements.html #itemPageback>*");
+    }
+
+    //SCALE TOOLBAR DOWN
     $("#toolbar").attr("id", "fakeToolbar");
     $("#userGreeting").css("transform", "translateX(465px)");
     $("#userGreeting").css("animation", "slideAcross 1s 1");
     $("#anotherFakeToolbar").attr("id", "fakeToolbar");
 
+    //POPULATE ITEM PAGE WITH DATA
     $.get("/getItemInfo", {
       _id: id
     }, (data) => {
@@ -294,6 +329,7 @@ function appender(id, link, name, price, clicks, uniqueClicks, which) {
       id = data.item._id;
     });
 
+    //ADD TO CART BUTTON CODE
     $("#addToCart").click(() => {
       $.post("/addToCart", {
         itemID: id
@@ -301,13 +337,11 @@ function appender(id, link, name, price, clicks, uniqueClicks, which) {
         alert(((data.status) ? "Item added to cart" : "Something went wrong"));
       });
     });
-
-
-    //window.location = window.location.href.split("/")[1] + "/item?id=" + id;
   });
 }
 var username, password;
 
+//USER DATA POPULATION
 function success(data) {
   if (data.redirect === "/") {
     window.location = window.location.href.split("/")[1] + "/";
